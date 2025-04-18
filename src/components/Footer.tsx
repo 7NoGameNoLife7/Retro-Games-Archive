3562757d627d8aefe36d9d8ead41aa73a7f982d8
DESCHAMPS BRUNO <bruno.deschamps07@gmail.com>
2025-04-18 13:36:53 +0200

    new vers de la page

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,8 +3,12 @@ import styles from "./../styles/footer.module.css";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <img className={styles.footerLogo} src="src\assets\images\logov1.png" alt="" />
-      <p className={styles.footer} >© 2025 Sébastien et Bruno</p>
+      <img
+        className={styles.footerLogo}
+        src="src/assets/images/logov1.png"
+        alt=""
+      />
+      <p className={styles.footerText}>© 2025 Bruno.D & Sébastien.A</p>
     </footer>
   );
 };
