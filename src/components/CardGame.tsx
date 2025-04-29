dd0a82cd2a3191e2c1a1c75ffd8444ec7cf37592
7NoGameNoLife7 <sebastien.zllegros@gmail.com>
2025-04-29 09:56:15 +0200

    finish

--- src/components/CardGame.tsx
+++ src/components/CardGame.tsx
@@ -1,5 +1,6 @@
 import { Game } from "../types/game";
 import styles from "../styles/cardGame.module.css";
+
 interface CardGameProps {
 	game: Game;
 }
@@ -8,14 +9,38 @@ function CardGame({ game }: CardGameProps) {
 	console.log(game);
 
 	const defaultImage = "src/assets/images/fail-1776388_640.webp";
-
 	const imageSrc = game.background_image ? game.background_image : defaultImage;
+
+	const getStars = (rating: number) => {
+		const maxStars = 5;
+		const starPercentage = rating / 5;
+		const fullStars = Math.round(starPercentage * maxStars);
+		return {
+			fullStars: fullStars,
+			emptyStars: maxStars - fullStars,
+		};
+	};
+
+	const { fullStars, emptyStars } = getStars(game.rating);
+
+	const starIcons = Array.from({ length: fullStars }, (_, index) => (
+		<span key={`full-${index}`} className={styles.starFull}>
+			★
+		</span>
+	)).concat(
+		Array.from({ length: emptyStars }, (_, index) => (
+			<span key={`empty-${index}`} className={styles.starEmpty}>
+				☆
+			</span>
+		)),
+	);
+
 	return (
 		<>
 			<figure className={styles.displayGame}>
 				<h3 className={styles.titleCard}>{game.name}</h3>
 				<img className="carouselImage" src={imageSrc} alt={game.name} />
-				<p>Note: {game.rating}</p>
+				<div className={styles.rating}>{starIcons}</div>
 			</figure>
 		</>
 	);
